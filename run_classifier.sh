a643c714cce4876f3c348edb025dbb2f9e21e32a
alexandr <alexandrslesarev16@gmail.com>
2019-02-13 23:02:34 +0300

    example added

--- run_classifier.sh
+++ run_classifier.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-is_base=false
+use_base=false
+create_base=false
 for key in ${BASH_ARGV[*]}
 do
     if [ $key = "create_base" ]
     then
-        is_base=true
-        break
+        create_base=true
+    fi
+    if [ $key = "use_base" ]
+    then
+        use_base=true
     fi
 done
 
-if $is_base
+if $create_base
 then
     ocaml create_bases.ml
+    ocaml bayessian_classifier.ml base
+elif $use_base
+then 
     ocaml bayessian_classifier.ml base
 else
     ocaml bayessian_classifier.ml
